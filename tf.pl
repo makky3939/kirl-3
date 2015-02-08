66a35316c648d6df653414e13e49954e00196a08
Masaki Kobayashi <makky@MakkysMacBookPro.local>
2015-02-08 21:29:44 +0900

    fix

--- tf.pl
+++ tf.pl
@@ -1,18 +1,5 @@
 use utf8;
 
-while($line = <STDIN>) {
-    chomp $line;
-    @row = split(/\s+/, $line);
-    $word = $row[1];
-    $word =~ s/(s$)|(er$)|(est$)|(ed$)|(ing$)//g;
-					$word =~ s/tion$/te/g;
-
-    print "$row[0] $word\n";
-}
-s1311495@unix02:~/perl/ex03$ emacs tf.pl
-s1311495@unix02:~/perl/ex03$ cat tf.pl
-    use utf8;
-
 $documents = ();
 
 while($line = <STDIN>) {
